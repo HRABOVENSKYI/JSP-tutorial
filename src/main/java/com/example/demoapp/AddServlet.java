7e4c40b96c28de2359cda480ec8c1c89d3405c6b
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-08-02 16:45:43 +0300

    Send redirect to Square servlet

--- src/main/java/com/example/demoapp/AddServlet.java
+++ src/main/java/com/example/demoapp/AddServlet.java
@@ -2,13 +2,10 @@ package com.example.demoapp;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
 
@@ -19,9 +16,9 @@ public class AddServlet extends HttpServlet {
 
         int k = i + j;
 
-        req.setAttribute("k", k);
+        resp.sendRedirect("sqr?k=" + k);
 
-        RequestDispatcher rd = req.getRequestDispatcher("sqr");
-        rd.forward(req, resp);
+//        RequestDispatcher rd = req.getRequestDispatcher("sqr");
+//        rd.forward(req, resp);
     }
 }
--- src/main/java/com/example/demoapp/SquareServlet.java
+++ src/main/java/com/example/demoapp/SquareServlet.java
@@ -1,6 +1,5 @@
 package com.example.demoapp;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ public class SquareServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        int k = (int) req.getAttribute("k");
+        int k = Integer.parseInt(req.getParameter("k"));
 
         k = k * k;
 
